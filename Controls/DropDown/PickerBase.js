ba8b993facb60f4c7d7a542a49eca547d3a848de
Jimmy Thomsen <jimmy@codemagic.dk>
2015-05-26 13:11:53 +0200

    PickerBase
    PickerBase: Reconstruction, lots of different changes

--- Controls/DropDown/PickerBase.js
+++ Controls/DropDown/PickerBase.js
@@ -1,54 +1,229 @@
-Fit.Controls.PickerBase = function()
+/// <container name="Fit.Controls.PickerBase">
+/// 	Class from which all Picker Controls inherit.
+/// 	Control developers must override: GetDomElement, Dispose.
+/// 	Overriding the following functions is optional:
+/// 	UpdateItemSelectionState, SetEventDispatcher, HandleEvent.
+/// 	Picker Control must fire OnItemSelectionChanged when an item's
+/// 	selection state is update, which is done by invoking
+/// 	this._internal.FireOnItemSelectionChanged(title:string, value:string, selected:boolean)
+/// </container>
+Fit.Controls.PickerBase = function(controlId)
 {
-	var me = this;
+	Fit.Validation.ExpectStringValue(controlId, true);
 
-	/// <function container="Fit.Controls.DropDown.PickerBase" name="GetDomElement" access="public" returns="DOMElement">
-	/// 	<description> Get DOMElement representing control </description>
+	// Support for Fit.Controls.Find(..)
+
+	if (Fit.Validation.IsSet(controlId) === true)
+	{
+		if (Fit._internal.ControlBase.Controls[controlId] !== undefined)
+			Fit.Validation.ThrowError("Control with ID '" + controlId + "' has already been defined - Control IDs must be unique!");
+
+		Fit._internal.ControlBase.Controls[controlId] = this;
+	}
+
+	// Private variables
+
+	var me = this;
+	var id = (controlId ? controlId : null);
+
+	var onShowHandlers = [];
+	var onHideHandlers = [];
+	var onChangeHandlers = [];
+
+	// ============================================
+	// Public
+	// ============================================
+
+	/// <function container="Fit.Controls.PickerBase" name="GetId" access="public" returns="string">
+	/// 	<description> Get unique Control ID </description>
+	/// </function>
+	this.GetId = function()
+	{
+		return id;
+	}
+
+	/// <function container="Fit.Controls.PickerBase" name="MaxHeight" access="public" returns="object">
+	/// 	<description> Get/set max height of control - returns object with Value (integer) and Unit (string) properties </description>
+	/// 	<param name="value" type="number" default="undefined"> If defined, max height is updated to specified value </param>
+	/// 	<param name="unit" type="string" default="undefined"> If defined, max height is updated to specified CSS unit, otherwise px is assumed </param>
+	/// </function>
+	this.MaxHeight = function(value, unit)
+    {
+		Fit.Validation.ExpectNumber(value, true);
+		Fit.Validation.ExpectStringValue(unit, true);
+
+		var elm = me.GetDomElement();
+
+		if (Fit.Validation.IsSet(value) === true)
+		{
+			elm.style.maxHeight = value + ((Fit.Validation.IsSet(unit) === true) ? unit : "px");
+		}
+
+		var res = { Value: parseInt(elm.style.maxHeight), Unit: "px" };
+		res.Unit = elm.style.maxHeight.replace(res.Value, "");
+
+		return res;
+    }
+
+	// ============================================
+	// Events fired by host control
+	// ============================================
+
+	/// <function container="Fit.Controls.PickerBase" name="OnShow" access="public">
+	/// 	<description>
+	/// 		Register event handler firered when picker control is shown in host control.
+	/// 		The following argument is passed to event handler function: Sender (PickerBase).
+	/// 	</description>
+	/// 	<param name="cb" type="function"> Event handler function </param>
+	/// </function>
+	this.OnShow = function(cb)
+	{
+		Fit.Validation.ExpectFunction(cb);
+		Fit.Array.Add(onShowHandlers, cb);
+	}
+
+	/// <function container="Fit.Controls.PickerBase" name="OnHide" access="public">
+	/// 	<description>
+	/// 		Register event handler firered when picker control is hidden in host control.
+	/// 		The following argument is passed to event handler function: Sender (PickerBase).
+	/// 	</description>
+	/// 	<param name="cb" type="function"> Event handler function </param>
+	/// </function>
+	this.OnHide = function(cb)
+	{
+		Fit.Validation.ExpectFunction(cb);
+		Fit.Array.Add(onHideHandlers, cb);
+	}
+
+	// ============================================
+	// Events fired by picker control itself
+	// ============================================
+
+	/// <function container="Fit.Controls.PickerBase" name="OnItemSelectionChanged" access="public">
+	/// 	<description>
+	/// 		Register event handler firered when item selection is changed.
+	/// 		The following arguments are passed to event handler function:
+	/// 		Sender (PickerBase), EventArgs (containing Title (string), Value (string), and Selected (boolean) properties).
+	/// 	</description>
+	/// 	<param name="cb" type="function"> Event handler function </param>
+	/// </function>
+    this.OnItemSelectionChanged = function(cb)
+    {
+		Fit.Validation.ExpectFunction(cb);
+		Fit.Array.Add(onChangeHandlers, cb);
+    }
+
+	// ============================================
+	// For derivatives - control developers
+	// ============================================
+
+	/// <function container="Fit.Controls.PickerBase" name="GetDomElement" access="public" returns="DOMElement">
+	/// 	<description>
+	/// 		Overridden by control developers (required).
+	/// 		Get DOMElement representing control.
+	/// 	</description>
 	/// </function>
 	this.GetDomElement = function()
 	{
 		Fit.Validation.ThrowError("Function not implemented");
 	}
 
-	/// <function container="Fit.Controls.DropDown.PickerBase" name="MaxHeight" access="public" returns="object">
-	/// 	<description> Get/set max height of control - returns object with Value and Unit properties </description>
-	/// 	<param name="val" type="number" default="undefined"> If defined, max height is updated to specified value </param>
-	/// 	<param name="unit" type="string" default="px"> If defined, max height is updated to specified CSS unit </param>
-	/// </function>
-	this.MaxHeight = function(val, unit)
-    {
-        Fit.Validation.ThrowError("Function not implemented");
-    }
-
-	/// <function container="Fit.Controls.DropDown.PickerBase" name="OnSelected" access="public">
+	/// <function container="Fit.Controls.PickerBase" name="UpdateItemSelection" access="public">
 	/// 	<description>
-	/// 		Add event handler fired when an item is selected.
-	/// 		Function receives two arguments: title (string), value (string).
+	/// 		Overridden by control developers (optional).
+	/// 		Host control invokes this function when an item's selection state is changed from host
+	/// 		control. Function must result in OnItemSelectionChanged event being fired if overridden,
+	/// 		which can be done using the following approach.
+	///
+	/// 		this._internal.FireOnItemSelectionChanged(title:string, value:string, selected:boolean);
+	///
+	/// 		Be aware that host control may pass information about items not found in picker,
+	/// 		e.g. when pasting items which may turn out not to be valid selections.
+	/// 		In this case OnItemSelectionChanged should not be fired.
 	/// 	</description>
-	/// 	<param name="cb" type="function"> Event handler function </param>
+	/// 	<param name="title" type="string"> Item title </param>
+	/// 	<param name="value" type="string"> Item value </param>
+	/// 	<param name="selected" type="boolean"> True if item was selected, False if item was deselected </param>
 	/// </function>
-    this.OnSelected = function(cb)
-    {
-        Fit.Validation.ThrowError("Function not implemented");
-    }
+	this.UpdateItemSelection = function(title, value, selected)
+	{
+		Fit.Validation.ExpectString(title);
+		Fit.Validation.ExpectString(value);
+		Fit.Validation.ExpectBoolean(selected);
+	}
 
-	/// <function container="Fit.Controls.DropDown.PickerBase" name="HandleEvent" access="public">
+	/// <function container="Fit.Controls.PickerBase" name="SetEventDispatcher" access="public" returns="DOMElement">
 	/// 	<description>
-	/// 		Have picker control process keyboard events to support keyboard
-	/// 		navigation with keys such as arrow up/down/left/right, enter, etc.
+	/// 		Overriden by control developers (optional).
+	/// 		Host control invokes this function, passing a reference to the
+	/// 		input control dispatching keyboard events using the HandleEvent function.
+	/// 		This function may be called multiple times with identical or different controls.
 	/// 	</description>
-	/// 	<param name="ev" type="Event"> Keyboard event to process </param>
+	/// 	<param name="control" type="DOMElement"> Event dispatcher control </param>
 	/// </function>
-	this.HandleEvent = function(ev)
+	this.SetEventDispatcher = function(control)
+	{
+		Fit.Validation.ExpectDomElement(control);
+	}
+
+	/// <function container="Fit.Controls.PickerBase" name="HandleEvent" access="public">
+	/// 	<description>
+	/// 		Overridden by control developers (optional).
+	/// 		Host control dispatches keyboard events to this function to allow
+	/// 		picker control to handle keyboard navigation with keys such as
+	/// 		arrow up/down/left/right, enter, space, etc.
+	/// 	</description>
+	/// 	<param name="e" type="Event" default="undefined"> Keyboard event to process </param>
+	/// </function>
+	this.HandleEvent = function(e)
+	{
+		Fit.Validation.ExpectEvent(e, true);
+	}
+
+	/// <function container="Fit.Controls.PickerBase" name="Dispose" access="public">
+	/// 	<description>
+	/// 		Overridden by control developers (required).
+	/// 		Destroys control to free up memory.
+	/// 	</description>
+	/// </function>
+	this.Dispose = function() // Must be overridden
 	{
 		Fit.Validation.ThrowError("Function not implemented");
 	}
 
+	// ============================================
+	// Private
+	// ============================================
 
-	// TODO: Get rid of these!
-	/*this.SetSelectionHandler = this.OnSelected;
-	this.GetElement = this.GetDomElement;
-	this.ResetSelection = this.Clear;
-	this.SetMaxHeight = function(h) { this.MaxHeight(h, "px"); };
-	this.GetSelected = this.Selected;*/
+	// Private members (must be public in order to be accessible to host control and controls inheriting from PickerBase)
+
+	this._internal = (this._internal ? this._internal : {});
+
+	this._internal.FireOnShow = function() // Called by Host Control
+	{
+		Fit.Array.ForEach(onShowHandlers, function(handler)
+		{
+			handler(me);
+		});
+	},
+
+	this._internal.FireOnHide = function() // Called by Host Control
+	{
+		Fit.Array.ForEach(onHideHandlers, function(handler)
+		{
+			handler(me);
+		});
+	},
+
+	this._internal.FireOnItemSelectionChanged = function(title, value, selected) // Called by Picker Control
+	{
+		Fit.Validation.ExpectString(title);
+		Fit.Validation.ExpectString(value);
+		Fit.Validation.ExpectBoolean(selected);
+
+		Fit.Array.ForEach(onChangeHandlers, function(handler)
+		{
+			handler(me, { Title: title, Value: value, Selected: selected });
+		});
+	}
 }
