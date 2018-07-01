768debfaba4f90ca1729557a2b3acb077e38fffa
Jimmy Thomsen <jimmy@codemagic.dk>
2018-07-01 12:18:04 +0200

    Made local debugging and testing easier.
    We can now start a debugging session by invoking:
    npm run debug
    This will start a local HTTP server and open the Debug page in the default browser.
    
    We can also start the automated tests by invoking:
    npm run test OR npm test
    This will start a local HTTP server and open the Test page in the default browser.
    
    Run the following command to install required dependencies first: npm install

---
+++ Debug/debug.js
@@ -0,0 +1,24 @@
+var view = new Fit.Template(true);
+view.LoadUrl("debug.html", function(sender)
+{
+    var item = null;
+
+    var input = new Fit.Controls.Input(Fit.Data.CreateGuid());
+    input.Value("Hello world");
+    item = view.Content.Controls.AddItem();
+    item.Control = input.GetDomElement();
+
+    var datepicker = new Fit.Controls.DatePicker(Fit.Data.CreateGuid());
+    datepicker.Date(new Date());
+    item = view.Content.Controls.AddItem();
+    item.Control = datepicker.GetDomElement();
+
+    var button = new Fit.Controls.Button();
+    button.Icon("cog");
+    button.Title("Configure");
+    item = view.Content.Controls.AddItem();
+    item.Control = button.GetDomElement();
+
+    view.Update();
+});
+view.Render(document.body);
