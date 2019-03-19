c721af31c2c5febbbf60c8902bedfcb47e2da72f
Jimmy Thomsen <jimmy@codemagic.dk>
2019-03-19 20:43:01 +0100

    Updated NPM package

--- Core/Core.js
+++ Core/Core.js
@@ -407,7 +407,7 @@ Fit._internal =
 {
 	Core:
 	{
-		VersionInfo: { Major: 1, Minor: 4, Patch: 4 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
+		VersionInfo: { Major: 1, Minor: 4, Patch: 5 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
 	}
 };
 
