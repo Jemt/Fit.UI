Jemt/Fit.UI

commit c8559b99f8db381d66b8fc1b1d8ff7c0b8463ec9

Performance improvements to TreeView & DropDown controls + various fixes
Fit.Array.HasItems: introduced HasItems(..) including tests - allows us to quickly determine whether an object array without a length has items or not.

Fit.Dom.IsVisible: Added tests.

PickerBase: OnItemSelectionChanged and OnItemSelectionChanging eventArgs now expose ProgrammaticallyChanged property. New ReportFocused(..) function let host control report focused state to picker controls.

DropDown: Implemented new features from PickerBase and greatly improved performance when working with large selections (issue #82).

WSDropDown: No longer closing DropDown automatically when triggering SelectAll - it took away control from the developer using the control.

ListView: Implemented changes from PickerBase (programmaticallyChanged flag).

TreeView: Added OnSelectAllComplete event. SelectAll no longer results in nodes being expanded automatically - it took away control from the developer using the control, and it hurt performance.
Fixed SelectAll which incorrectly selected targetNode - only its children should be selected.
AddChild(..) on TreeView and TreeViewNode now lets us add a child at a specific position.
Added support for "behavioral nodes" which are nodes that are not consider data, but rather performs some logic (invokes a callback) when selected. Various other improvements and fixes.

WSTreeView: Added support for behavioral nodes. Renamed internal function fireOnDataLoadedAndPopulated to fireOnDataLoadedToResumeProcessQueue for clarity. Fixed incorrect event order (issue #84) for SelectAll, EnsureData, and Reload. Fixed bug resulting in SelectAll reloading data (SelectAllMode set to Instantly), even though it was already loaded (missing WSDone flag in Reload function).

Controls/DropDown/DropDown.js
@@ -15,6 +15,7 @@ Fit.Controls.DropDown = function(ctlId)
 
 	var me = this;								// Access to members from event handlers (where "this" may have a different meaning)
 	var itemContainer = null;					// Container for selected items and input fields
+	var itemCollection = {};					// Indexed collection for selected items (for fast lookup)
 	var arrow = null;							// Arrow button used to open/close drop down menu
 	var hidden = null;							// Area used to hide DOM elements (e.g span used to calculate width of input fields)
 	var spanFitWidth = null;					// Span element used to calculate text width - used to dynamically control width of input fields
@@ -274,6 +275,24 @@ Fit.Controls.DropDown = function(ctlId)
 			}
 		});
 
+		// PickerBase - make picker aware of focused state of host control
+
+		me.OnFocus(function()
+		{
+			if (picker !== null)
+			{
+				picker._internal.ReportFocused(true);
+			}
+		});
+
+		me.OnBlur(function()
+		{
+			if (picker !== null)
+			{
+				picker._internal.ReportFocused(false);
+			}
+		});
+
 		// Append elements to the DOM
 
 		Fit.Dom.Add(hidden, spanFitWidth);
@@ -304,10 +323,12 @@ Fit.Controls.DropDown = function(ctlId)
 		{
 			invalidMessage = msg;
 
-			Fit.Array.ForEach(getSelectionElements(), function(selection)
+			Fit.Array.ForEach(itemCollection, function(key)
 			{
-				if (Fit.Dom.HasClass(selection, "FitUiControlDropDownInvalid") === true)
-					Fit.Dom.Attribute(selection, "title", invalidMessage);
+				var selection = itemCollection[key];
+
+				if (selection.Valid === false)
+					Fit.Dom.Attribute(selection.DomElement, "title", invalidMessage);
 			});
 		}
 
@@ -444,7 +465,7 @@ Fit.Controls.DropDown = function(ctlId)
 		if (Fit.Validation.IsSet(val) === true)
 		{
 			orgSelections = [];
-			var fireChange = (getSelectionElements().length > 0 || val !== ""); // Fire OnChange if current selections are cleared, and/or if new selections are set
+			var fireChange = (Fit.Array.HasItems(itemCollection) === true || val !== ""); // Fire OnChange if current selections are cleared, and/or if new selections are set
 
 			me._internal.ExecuteWithNoOnChange(function()
 			{
@@ -552,7 +573,7 @@ Fit.Controls.DropDown = function(ctlId)
 			Fit.Events.RemoveHandler(document, eventId);
 		});
 
-		me = itemContainer = arrow = hidden = spanFitWidth = txtPrimary = txtCssWidth = txtActive = txtEnabled = dropDownMenu = picker = orgSelections = invalidMessage = initialFocus = maxHeight = prevValue = focusAssigned = visibilityObserverId = widthObserverId = tabOrderObserverId = partiallyHidden = closeHandlers = dropZone = isMobile = focusInputOnMobile = detectBoundaries = onInputChangedHandlers = onPasteHandlers = onOpenHandlers = onCloseHandlers = suppressUpdateItemSelectionState = suppressOnItemSelectionChanged = clearTextSelectionOnInputChange = prevTextSelection = textSelectionCallback = null;
+		me = itemContainer = itemCollection = arrow = hidden = spanFitWidth = txtPrimary = txtCssWidth = txtActive = txtEnabled = dropDownMenu = picker = orgSelections = invalidMessage = initialFocus = maxHeight = prevValue = focusAssigned = visibilityObserverId = widthObserverId = tabOrderObserverId = partiallyHidden = closeHandlers = dropZone = isMobile = focusInputOnMobile = detectBoundaries = onInputChangedHandlers = onPasteHandlers = onOpenHandlers = onCloseHandlers = suppressUpdateItemSelectionState = suppressOnItemSelectionChanged = clearTextSelectionOnInputChange = prevTextSelection = textSelectionCallback = null;
 
 		base();
 	});
@@ -674,6 +695,7 @@ Fit.Controls.DropDown = function(ctlId)
 		}
 
 		pickerControl._internal.InitializePicker();
+		pickerControl._internal.ReportFocused(me.Focused());
 
 		picker = pickerControl;
 		Fit.Dom.Add(dropDownMenu, picker.GetDomElement());
@@ -747,7 +769,7 @@ Fit.Controls.DropDown = function(ctlId)
 					txt = txtPrimary;
 			}
 
-			if (isMobile === false || focusInputOnMobile === true)
+			if (eventArgs.ProgrammaticallyChanged === false && (isMobile === false || focusInputOnMobile === true))
 			{
 				focusAssigned = true; // Clicking the picker causes blur to fire for input fields in drop down which changes focusAssigned to false - it must be true for focusInput(..) to assign focus
 				focusInput(((txt !== null) ? txt : txtActive));
@@ -816,7 +838,7 @@ Fit.Controls.DropDown = function(ctlId)
 
 			try // Make sure we can set suppressOnItemSelectionChanged false again, so drop down remains in a functioning state
 			{
-				res = picker.UpdateItemSelection(value, true);
+				res = picker.UpdateItemSelection(value, true, me.Focused() === false);
 			}
 			catch (err) { error = err; }
 
@@ -851,7 +873,7 @@ Fit.Controls.DropDown = function(ctlId)
 			if (error !== null)
 				Fit.Validation.ThrowError(error);
 
-			if (getSelectionElements().length > 0) // A picker prevented selected item from being removed
+			if (Fit.Array.HasItems(itemCollection) === true) // A picker prevented selected item from being removed
 				return;
 		}
 
@@ -945,7 +967,8 @@ Fit.Controls.DropDown = function(ctlId)
 		}
 
 		// Add title and delete button to title box
-		Fit.Dom.Add(item, document.createTextNode(Fit.String.StripHtml(title)));
+		var titleWithoutHtml = Fit.String.StripHtml(title);
+		Fit.Dom.Add(item, document.createTextNode(titleWithoutHtml));
 		Fit.Dom.Add(item, cmdDelete);
 
 		// Add elements to item container
@@ -965,6 +988,9 @@ Fit.Controls.DropDown = function(ctlId)
 			before = txtPrimary;
 
 		itemContainer.insertBefore(container, before);
+
+		var itemObject = createItemObject(titleWithoutHtml, value, valid !== false, item); //convertItemElementToItemObject(item);
+		itemCollection[itemObject.Value] = itemObject;
 
 		// Clear input control value
 
@@ -1052,18 +1078,18 @@ Fit.Controls.DropDown = function(ctlId)
 	{
 		Fit.Validation.ExpectBoolean(includeInvalid, true);
 
-		var selections = Fit.Array.ToArray(itemContainer.children); // Convert NodeList to JS Array, since RemoveAt takes an instance of Array
-
-		// Remove two last elements from array which are not selections (primary input field and arrow button)
-		Fit.Array.RemoveAt(selections, selections.length - 1);
-		Fit.Array.RemoveAt(selections, selections.length - 1);
-
 		var toReturn = [];
-		Fit.Array.ForEach(selections, function(selection)
+
+		Fit.Array.ForEach(itemCollection, function(key)
 		{
-			if (includeInvalid === true || Fit.Dom.HasClass(selection.children[1], "FitUiControlDropDownInvalid") === false)
-				Fit.Array.Add(toReturn, { Title: Fit.Dom.Text(selection.children[1]), Value: decode(Fit.Dom.Data(selection.children[1], "value")), Valid: !Fit.Dom.HasClass(selection.children[1], "FitUiControlDropDownInvalid") });
+			var selection = itemCollection[key];
+
+			if (includeInvalid === true || selection.Valid === true)
+			{
+				Fit.Array.Add(toReturn, { Title: selection.Title, Value: selection.Value, Valid: selection.Valid });
+			}
 		});
+
 		return toReturn;
 	}
 
@@ -1074,17 +1100,15 @@ Fit.Controls.DropDown = function(ctlId)
 	this.GetSelectionByValue = function(val)
 	{
 		Fit.Validation.ExpectString(val);
-
-		var found = null;
-		Fit.Array.ForEach(me.GetSelections(), function(selection)
+		
+		var selection = itemCollection[val] || null;
+		
+		if (selection !== null)
 		{
-			if (selection.Value === val)
-			{
-				found = selection;
-				return false; // Break loop
-			}
-		});
-		return found;
+			return { Title: selection.Title, Value: selection.Value, Valid: selection.Valid };
+		}
+
+		return null;
 	}
 
 	/// <function container="Fit.Controls.DropDown" name="ClearSelections" access="public">
@@ -1097,8 +1121,10 @@ Fit.Controls.DropDown = function(ctlId)
 
 		me._internal.ExecuteWithNoOnChange(function() // picker.UpdateItemSelection results in OnChange being fired
 		{
-			Fit.Array.ForEach(getSelectionElements(), function(selection)
+			Fit.Array.ForEach(itemCollection, function(key)
 			{
+				var selection = itemCollection[key];
+
 				if (picker !== null)
 				{
 					// Notice: Picker fires OnItemSelectionChanged when picker.UpdateItemSelection(..) is invoked
@@ -1114,7 +1140,7 @@ Fit.Controls.DropDown = function(ctlId)
 
 					try // Make sure we can set suppressOnItemSelectionChanged false again, so drop down remains in a functioning state
 					{
-						var res = picker.UpdateItemSelection(decode(Fit.Dom.Data(selection, "value")), false); // OnItemSelectionChanging and OnItemSelectionChanged are fired if picker recognizes item, causing it to be removed in drop down's OnItemSelectionChanged handler (unless canceled, in which case False is returned)
+						var res = picker.UpdateItemSelection(selection.Value, false, me.Focused() === false); // OnItemSelectionChanging and OnItemSelectionChanged are fired if picker recognizes item, causing it to be removed in drop down's OnItemSelectionChanged handler (unless canceled, in which case False is returned)
 					}
 					catch (err) { error = err; }
 
@@ -1123,14 +1149,14 @@ Fit.Controls.DropDown = function(ctlId)
 					if (error !== null)
 						Fit.Validation.ThrowError(error);
 
-					if (res !== false && selection.parentElement.parentElement !== null)
+					if (res !== false && selection.DomElement.parentElement.parentElement !== null)
 					{
 						// Element was not removed (still rooted in DOM), because picker did not recognize
 						// it (did not fire OnItemSelectionChanged) which would otherwise have triggered Drop
 						// Down's OnItemSelectionChanged handler, which in turn would have called RemoveSelection.
 						// And we know it did not cancel change since picker.UpdateItemSelection did not return False.
 						// It is fine to remove item.
-						Fit.Dom.Remove(selection.parentElement);
+						Fit.Dom.Remove(selection.DomElement.parentElement);
 					}
 
 					if (res !== false)
@@ -1138,10 +1164,12 @@ Fit.Controls.DropDown = function(ctlId)
 				}
 				else
 				{
-					Fit.Dom.Remove(selection.parentElement);
+					Fit.Dom.Remove(selection.DomElement.parentElement);
 					fireEvent = true;
 				}
 			});
+
+			itemCollection = {};
 		});
 
 		focusAssigned = wasFocused;
@@ -1184,7 +1212,7 @@ Fit.Controls.DropDown = function(ctlId)
 
 			try // Make sure we can set suppressOnItemSelectionChanged false again, so drop down remains in a functioning state
 			{
-				res = picker.UpdateItemSelection(value, false);
+				res = picker.UpdateItemSelection(value, false, me.Focused() === false);
 			}
 			catch (err) { error = err; }
 
@@ -1200,20 +1228,15 @@ Fit.Controls.DropDown = function(ctlId)
 		var found = null;
 		var txt = null;
 
-		Fit.Array.ForEach(getSelectionElements(), function(selection)
-		{
-			if (decode(Fit.Dom.Data(selection, "value")) === value)
-			{
-				if (me.MultiSelectionMode() === true && selection.parentElement.nextSibling !== null && selection.parentElement.nextSibling !== txtPrimary)
-					txt = selection.parentElement.nextSibling.children[0];
+		var itemObject = itemCollection[value] || null;
 
-				found = selection.parentElement;
-				return false;
-			}
-		});
-
-		if (found === null)
+		if (itemObject === null)
 			return;
+		
+		if (me.MultiSelectionMode() === true && itemObject.DomElement.parentElement.nextSibling !== null && itemObject.DomElement.parentElement.nextSibling !== txtPrimary)
+			txt = itemObject.DomElement.parentElement.nextSibling.children[0];
+
+		found = itemObject.DomElement.parentElement;
 
 		if (me.TextSelectionMode() === false)
 		{
@@ -1233,6 +1256,7 @@ Fit.Controls.DropDown = function(ctlId)
 		}
 
 		Fit.Dom.Remove(found);
+		delete itemCollection[value];
 
 		if (me.MultiSelectionMode() === false)
 		{
@@ -1240,7 +1264,7 @@ Fit.Controls.DropDown = function(ctlId)
 		}
 		else
 		{
-			if (getSelectionElements().length === 0)
+			if (Fit.Array.HasItems(itemCollection) === false)
 				optimizeTabOrder(); // Make sure txtPrimary can receive focus using Tab or Shift+Tab
 		}
 
@@ -1935,15 +1959,38 @@ Fit.Controls.DropDown = function(ctlId)
 		return txt;
 	}
 
-	function getSelectionElements() // Return spans containing Title and Value (also include elements marked as invalid selections)
+	function createItemObject(title, value, valid, domElement)
 	{
-		return itemContainer.querySelectorAll("span[data-value]");
+		Fit.Validation.ExpectString(title);
+		Fit.Validation.ExpectString(value);
+		Fit.Validation.ExpectBoolean(valid);
+		Fit.Validation.ExpectDomElement(domElement);
+
+		return { Title: title, Value: value, Valid: valid, DomElement: domElement };
 	}
+	/*function convertItemElementToItemObject(itemElm)
+	{
+		Fit.Validation.ExpectDomElement(itemElm);
+		return { Title: Fit.Dom.Text(itemElm), Value: decode(Fit.Dom.Data(itemElm, "value")), Valid: Fit.Dom.HasClass(itemElm, "FitUiControlDropDownInvalid") === false, DomElement: itemElm };
+	}*/
 
 	function getSelectionElementByValue(value)
 	{
 		Fit.Validation.ExpectString(value);
-		return itemContainer.querySelector("span[data-value='" + encode(value) + "']");
+
+		var item = itemCollection[value] || null;
+		return item !== null ? item.DomElement : null;
+	}
+
+	function getFirstSelectionElement()
+	{
+		var first = null;
+		Fit.Array.ForEach(itemCollection, function(key)
+		{
+			first = itemCollection[key].DomElement;
+			return false;
+		})
+		return first;
 	}
 
 	function fitWidthToContent(input, val) // Set width of input field equivalent to its content
@@ -2085,8 +2132,8 @@ Fit.Controls.DropDown = function(ctlId)
 
 		if (me.MultiSelectionMode() === false) // Single Selection Mode
 		{
-			var selections = getSelectionElements();
-			partiallyHidden = ((selections.length > 0 && selections[0].parentElement.offsetWidth + 1 > Fit.Dom.GetInnerWidth(itemContainer)) ? selections[0] : null); // Adding 1px to offsetWidth - otherwise right aligned cursor may become hidden behind drop down arrow box
+			var firstSelection = getFirstSelectionElement();
+			partiallyHidden = ((firstSelection !== null && firstSelection.parentElement.offsetWidth + 1 > Fit.Dom.GetInnerWidth(itemContainer)) ? firstSelection : null); // Adding 1px to offsetWidth - otherwise right aligned cursor may become hidden behind drop down arrow box
 
 			var inputs = itemContainer.querySelectorAll("input");
 			Fit.Array.ForEach(inputs, function(input)
@@ -2101,8 +2148,10 @@ Fit.Controls.DropDown = function(ctlId)
 		{
 			partiallyHidden = null;
 
-			Fit.Array.ForEach(getSelectionElements(), function(item)
+			Fit.Array.ForEach(itemCollection, function(key)
 			{
+				var item = itemCollection[key].DomElement;
+
 				if (item.parentElement.offsetWidth + 1 > Fit.Dom.GetInnerWidth(itemContainer)) // Adding 1px to offsetWidth - otherwise right aligned cursor may become hidden behind drop down arrow box
 					item.nextSibling.tabIndex = -1; // Item is partially hidden - disable right search field
 				else

commit 251c9a39495f27941965652799bdb5b32c5ff730

#92: DropDown now returns selected items in correct order.
Bug was introduced in commit c8559b99f8db381d66b8fc1b1d8ff7c0b8463ec9

Also added Fit.Array.Move function and related tests.

Controls/DropDown/DropDown.js
@@ -16,6 +16,7 @@ Fit.Controls.DropDown = function(ctlId)
 	var me = this;								// Access to members from event handlers (where "this" may have a different meaning)
 	var itemContainer = null;					// Container for selected items and input fields
 	var itemCollection = {};					// Indexed collection for selected items (for fast lookup)
+	var itemCollectionOrdered = [];				// Ordered item collection (item order can be changed using drag and drop)
 	var itemDropZones = {};						// Indexed collection of dropzones used to enable item dragging/dropping
 	var arrow = null;							// Arrow button used to open/close drop down menu
 	var hidden = null;							// Area used to hide DOM elements (e.g span used to calculate width of input fields)
@@ -1067,6 +1068,7 @@ Fit.Controls.DropDown = function(ctlId)
 
 		var itemObject = createItemObject(titleWithoutHtml, value, valid !== false, item); //convertItemElementToItemObject(item);
 		itemCollection[itemObject.Value] = itemObject;
+		itemCollectionOrdered.push(itemObject);
 
 		// Clear input control value
 
@@ -1157,10 +1159,8 @@ Fit.Controls.DropDown = function(ctlId)
 
 		var toReturn = [];
 
-		Fit.Array.ForEach(itemCollection, function(key)
+		Fit.Array.ForEach(itemCollectionOrdered, function(selection)
 		{
-			var selection = itemCollection[key];
-
 			if (includeInvalid === true || selection.Valid === true)
 			{
 				Fit.Array.Add(toReturn, { Title: selection.Title, Value: selection.Value, Valid: selection.Valid });
@@ -1198,10 +1198,8 @@ Fit.Controls.DropDown = function(ctlId)
 
 		me._internal.ExecuteWithNoOnChange(function() // picker.UpdateItemSelection results in OnChange being fired
 		{
-			Fit.Array.ForEach(itemCollection, function(key)
+			Fit.Array.ForEach(itemCollectionOrdered, function(selection)
 			{
-				var selection = itemCollection[key];
-
 				if (picker !== null)
 				{
 					// Notice: Picker fires OnItemSelectionChanged when picker.UpdateItemSelection(..) is invoked
@@ -1252,6 +1250,7 @@ Fit.Controls.DropDown = function(ctlId)
 			});
 
 			itemCollection = {};
+			itemCollectionOrdered = [];
 			itemDropZones = {};
 		});
 
@@ -1339,6 +1338,7 @@ Fit.Controls.DropDown = function(ctlId)
 		}
 
 		Fit.Dom.Remove(found);
+		Fit.Array.Remove(itemCollectionOrdered, itemObject);
 		delete itemCollection[value];
 
 		itemDropZones[value].Dispose();
@@ -2067,13 +2067,12 @@ Fit.Controls.DropDown = function(ctlId)
 
 	function getFirstSelectionElement()
 	{
-		var first = null;
-		Fit.Array.ForEach(itemCollection, function(key)
+		if (itemCollectionOrdered.length > 0)
 		{
-			first = itemCollection[key].DomElement;
-			return false;
-		})
-		return first;
+			return itemCollectionOrdered[0].DomElement;
+		}
+
+		return null;
 	}
 
 	function fitWidthToContent(input, val) // Set width of input field equivalent to its content
@@ -2237,9 +2236,9 @@ Fit.Controls.DropDown = function(ctlId)
 		{
 			partiallyHidden = null;
 
-			Fit.Array.ForEach(itemCollection, function(key)
+			Fit.Array.ForEach(itemCollectionOrdered, function(itemObject)
 			{
-				var item = itemCollection[key].DomElement;
+				var item = itemObject.DomElement;
 
 				if (item.parentElement.offsetWidth + 1 > Fit.Dom.GetInnerWidth(itemContainer)) // Adding 1px to offsetWidth - otherwise right aligned cursor may become hidden behind drop down arrow box
 					item.nextSibling.tabIndex = -1; // Item is partially hidden - disable right search field
@@ -2467,7 +2466,14 @@ Fit.Controls.DropDown = function(ctlId)
 			focusAssigned = true;
 			focusInput(txtPrimary);
 
+			var draggableIndex = Fit.Dom.GetIndex(draggable.GetDomElement());
+			var dropzoneIndex =  Fit.Dom.GetIndex(dropzone.GetDomElement());
+			var itemObject = itemCollectionOrdered[draggableIndex];
+
+			Fit.Array.RemoveAt(itemCollectionOrdered, draggableIndex);
+			Fit.Array.Insert(itemCollectionOrdered, (draggableIndex < dropzoneIndex ? dropzoneIndex : dropzoneIndex + 1), itemObject);
 			Fit.Dom.InsertAfter(dropzone.GetDomElement(), draggable.GetDomElement());
+
 			fireChange = true;
 		}
 		else if (Fit.Dom.Data(dropzone.GetDomElement(), "dropping") === "left" && dropzone.GetDomElement().previousSibling !== draggable.GetDomElement())
@@ -2476,7 +2482,14 @@ Fit.Controls.DropDown = function(ctlId)
 			focusAssigned = true;
 			focusInput(txtPrimary);
 
+			var draggableIndex = Fit.Dom.GetIndex(draggable.GetDomElement());
+			var dropzoneIndex =  Fit.Dom.GetIndex(dropzone.GetDomElement());
+			var itemObject = itemCollectionOrdered[draggableIndex];
+
+			Fit.Array.RemoveAt(itemCollectionOrdered, draggableIndex);
+			Fit.Array.Insert(itemCollectionOrdered, (draggableIndex < dropzoneIndex ? dropzoneIndex - 1 : dropzoneIndex), itemObject);
 			Fit.Dom.InsertBefore(dropzone.GetDomElement(), draggable.GetDomElement());
+
 			fireChange = true;
 		}
 

Core/Array.js
@@ -210,6 +210,29 @@ Fit.Array.Insert = function(arr, idx, obj) // obj not validated - passing any ob
     arr.splice(idx, 0, obj);
 }
 
+/// <function container="Fit.Array" name="Move" access="public" static="true">
+/// 	<description> Move object within array from one position to another </description>
+/// 	<param name="arr" type="array"> Array to manipulate </param>
+/// 	<param name="fromIdx" type="integer"> Position of object to move </param>
+/// 	<param name="ToIdx" type="integer"> New object position </param>
+/// </function>
+Fit.Array.Move = function(arr, fromIdx, toIdx)
+{
+	Fit.Validation.ExpectArray(arr);
+	Fit.Validation.ExpectInteger(fromIdx);
+	Fit.Validation.ExpectInteger(toIdx);
+
+	if (fromIdx < arr.length)
+	{
+		// If toIdx is below 0 (zero) the object will be added at the beginning of the array.
+		// If toIdx is greater than the length of the array, the object will be added at the end of the array.
+
+		var obj = arr[fromIdx];
+		Fit.Array.RemoveAt(arr, fromIdx);
+		Fit.Array.Insert(arr, toIdx > 0 ? toIdx : 0, obj);
+	}
+}
+
 /// <function container="Fit.Array" name="Merge" access="public" static="true" returns="array">
 /// 	<description> Merge/join two arrays </description>
 /// 	<param name="arr1" type="array"> Array 1 to merge with array 2 </param>

build/npm/types/index.d.ts
@@ -83,19 +83,19 @@ declare namespace Fit
 		*/
 		public static Copy(arr:any[]):any[];
 		/**
-		* Returns number of elements in object array
-		* @function Count
-		* @param {any} obj - Object array to count elements within
-		* @returns number
-		*/
-		public static Count(obj:any):number;
-		/**
 		* Returns number of elements in collection
 		* @function Count
 		* @param {any[]} arr - Collection to count elements within
 		* @returns number
 		*/
 		public static Count(arr:any[]):number;
+		/**
+		* Returns number of elements in object array
+		* @function Count
+		* @param {any} obj - Object array to count elements within
+		* @returns number
+		*/
+		public static Count(obj:any):number;
 		/**
 		* Iterate objects in collection and pass each object to provided callback.
 		Callback is expected to return any children supposed to be iterated too, or Null
@@ -153,17 +153,17 @@ declare namespace Fit
 		/**
 		* Returns True if collection has items, otherwise False
 		* @function HasItems
-		* @param {any[]} arr - Collection to investigate
-		* @returns boolean
-		*/
-		public static HasItems(arr:any[]):boolean;
-		/**
-		* Returns True if collection has items, otherwise False
-		* @function HasItems
 		* @param {any} obj - Object array to investigate
 		* @returns boolean
 		*/
 		public static HasItems(obj:any):boolean;
+		/**
+		* Returns True if collection has items, otherwise False
+		* @function HasItems
+		* @param {any[]} arr - Collection to investigate
+		* @returns boolean
+		*/
+		public static HasItems(arr:any[]):boolean;
 		/**
 		* Insert object into array at specified index
 		* @function Insert
@@ -180,6 +180,14 @@ declare namespace Fit
 		* @returns any[]
 		*/
 		public static Merge(arr1:any[], arr2:any[]):any[];
+		/**
+		* Move object within array from one position to another
+		* @function Move
+		* @param {any[]} arr - Array to manipulate
+		* @param {number} fromIdx - Position of object to move
+		* @param {number} ToIdx - New object position
+		*/
+		public static Move(arr:any[], fromIdx:number, ToIdx:number):void;
 		/**
 		* Recursively iterates through objects in array and passes each object to the provided callback function.
 		Returns boolean indicating whether recursion was carried through (True) or interrupted (False).
@@ -6419,6 +6427,13 @@ declare namespace Fit
 		*/
 		public static PreventDefault(e?:Event):boolean;
 		/**
+		* Remove event handler given by Event ID returned from Fit.Events.AddHandler(..)
+		* @function RemoveHandler
+		* @param {HTMLElement} element - EventTarget (e.g. Window or DOMElement) from which event handler is removed
+		* @param {number} eventId - Event ID identifying handler to remove
+		*/
+		public static RemoveHandler(element:HTMLElement, eventId:number):void;
+		/**
 		* Remove event handler for specified event on given EventTarget
 		* @function RemoveHandler
 		* @param {HTMLElement} element - EventTarget (e.g. Window or DOMElement) from which event handler is removed
@@ -6436,19 +6451,6 @@ declare namespace Fit
 		*/
 		public static RemoveHandler(element:HTMLElement, event:string, eventFunction:Function):void;
 		/**
-		* Remove event handler given by Event ID returned from Fit.Events.AddHandler(..)
-		* @function RemoveHandler
-		* @param {HTMLElement} element - EventTarget (e.g. Window or DOMElement) from which event handler is removed
-		* @param {number} eventId - Event ID identifying handler to remove
-		*/
-		public static RemoveHandler(element:HTMLElement, eventId:number):void;
-		/**
-		* Remove mutation observer by ID
-		* @function RemoveMutationObserver
-		* @param {number} id - Observer ID returned from AddMutationObserver(..) function
-		*/
-		public static RemoveMutationObserver(id:number):void;
-		/**
 		* Remove mutation observer
 		* @function RemoveMutationObserver
 		* @param {HTMLElement} elm - DOMElement being observed
@@ -6456,6 +6458,12 @@ declare namespace Fit
 		* @param {boolean} [deep=undefined] - If defined, observer must have been registered with the same deep value to be removed
 		*/
 		public static RemoveMutationObserver(elm:HTMLElement, obs:Function, deep?:boolean):void;
+		/**
+		* Remove mutation observer by ID
+		* @function RemoveMutationObserver
+		* @param {number} id - Observer ID returned from AddMutationObserver(..) function
+		*/
+		public static RemoveMutationObserver(id:number):void;
 		/**
 		* Completely suppress event which is equivalent of
 		calling both PreventDefault(e) and StopPropagation(e).

Test/Core/Array.js
@@ -368,11 +368,12 @@ Tests.ArrayManipulation = function()
 	var arr3 = [];
 	var arr4 = [];
 	var arr5 = [];
+	var arr6 = [];
 
 	var arr2IdxExisting = -1;
 	var arr2IdxNonExisting = -1;
 
-	this.Description = "Arrays can be manipulated (Add/Insert/Remove/RemoveAt/Clear/GetIndex/Contains)";
+	this.Description = "Arrays can be manipulated (Add/Insert/Remove/RemoveAt/Clear/GetIndex/Contains/Move)";
 
 	this.Execute = function()
 	{
@@ -405,6 +406,14 @@ Tests.ArrayManipulation = function()
 		Fit.Array.Add(arr5, "B");
 		Fit.Array.Add(arr5, "C");
 		Fit.Array.Clear(arr5); // Result: []
+
+		Fit.Array.Add(arr6, "A");
+		Fit.Array.Add(arr6, "B");
+		Fit.Array.Add(arr6, "C");		// Result: [A, B, C]
+		Fit.Array.Move(arr6, 0, 2);		// Result: [B, C, A]
+		Fit.Array.Move(arr6, 1, 999);	// Result: [B, A, C]
+		Fit.Array.Move(arr6, 2, -1);	// Result: [C, B, A]
+		Fit.Array.Move(arr6, 3, 1);		// Result: [C, B, A] (nothing changed, no item on position 3)
 	}
 
 	this.Assertions =
@@ -489,6 +498,14 @@ Tests.ArrayManipulation = function()
 			GetResult: function()
 			{
 				return (arr5.length === 0 && arr5[0] === undefined);
+			}
+		},
+		{
+			Message: "Move function was able to move elements around",
+			Expected: true,
+			GetResult: function()
+			{
+				return arr6[0] === "C" && arr6[1] === "B" && arr6[2] === "A";
 			}
 		}
 	]