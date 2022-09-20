30dcab9722c7f7b961cccf1369612eb66d6d92b1
Jimmy Thomsen <jit@flowit.dk>
2022-09-20 16:30:18 +0200

    Updated NPM package

--- Core/Core.js
+++ Core/Core.js
@@ -612,7 +612,7 @@ Fit._internal =
 {
 	Core:
 	{
-		VersionInfo: { Major: 2, Minor: 11, Patch: 15 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
+		VersionInfo: { Major: 2, Minor: 11, Patch: 16 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
 	}
 };
 
