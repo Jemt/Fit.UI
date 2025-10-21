b93c2969b3e3643cd2999473f3f877435f9508db
Jimmy Thomsen <jit@projectflow.dk>
2025-10-21 09:08:49 +0200

    HTML editor: Added missing Norwegian translations for base64image plugin

---
+++ Resources/CKEditor/plugins/base64image/lang/no.js
@@ -0,0 +1,8 @@
+// Translated by Jimmy Thomsen for CKEditor in Fit.UI (https://fitui.org)
+CKEDITOR.plugins.setLang("base64image","no",{
+	"alt": "Alternativ tekst",
+	"lockRatio": "Lås sideforhold",
+	"vSpace": "Vertikal marg",
+	"hSpace": "Horisontal marg",
+	"border": "Ramme"
+});
