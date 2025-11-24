e7569f2fc425a108bd7293d8c0e6b29bb48f6eb3
Jimmy Thomsen <jit@projectflow.dk>
2025-11-24 13:08:27 +0100

    Updated NPM package
    Signed-off-by: Jimmy Thomsen <jit@projectflow.dk>

--- Core/Core.js
+++ Core/Core.js
@@ -612,7 +612,7 @@ Fit._internal =
 {
 	Core:
 	{
-		VersionInfo: { Major: 3, Minor: 3, Patch: 2 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
+		VersionInfo: { Major: 3, Minor: 3, Patch: 3 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
 	}
 };
 
